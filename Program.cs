9532e4ccfafc2a6ad62068f07e087ef8bc3833d4
Jamie Myland <jamie.myland@aru.ac.uk>
2023-01-19 14:22:59 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace learningcsharp1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string username;
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please enter your name");
+        username = Console.ReadLine();
+        Console.WriteLine($"Goodbye, {username}!");
+        Console.ReadKey(true);
+    }
+}
